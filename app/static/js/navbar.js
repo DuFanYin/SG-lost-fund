c9ff693b1995a38910cb296d5b4ed13132e1aa67
ljwitsme <jiawenlee5@gmail.com>
2024-10-29 14:27:47 +0800

    remove duplicated firebaseconfig

--- app/static/js/navbar.js
+++ app/static/js/navbar.js
@@ -1,24 +1,3 @@
-// Firebase configuration and initialization
-const firebaseConfig = {
-    apiKey: "AIzaSyAUZYEkqQSsEVM7rMCLqaEKoibGPiP_YJI",
-    authDomain: "wad2project-db69b.firebaseapp.com",
-    databaseURL: "https://wad2project-db69b-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "wad2project-db69b",
-    storageBucket: "wad2project-db69b.appspot.com",
-    messagingSenderId: "262163048895",
-    appId: "1:262163048895:web:5ab7dd89cf3bc6daaad90a",
-    measurementId: "G-PKT1RMGB01"
-  };
-
-// Initialize Firebase
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-// Initialize Firestore
-const db = firebaseApp.firestore();
-
-// Now you can use db.collection() in your Vue app
-
-
 const app = Vue.createApp({
     data() {
         return {
